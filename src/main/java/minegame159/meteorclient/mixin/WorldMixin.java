7edcb41db0c3a0936349992ff14af9faf374b05c
SpaceClouds42 <spaceclouds42@gmail.com>
2021-01-08 23:31:03 -0500

    seed cracker???!!!

--- src/main/java/minegame159/meteorclient/mixin/WorldMixin.java
+++ src/main/java/minegame159/meteorclient/mixin/WorldMixin.java
@@ -2,13 +2,23 @@ package minegame159.meteorclient.mixin;
 
 import minegame159.meteorclient.utils.world.IWorld;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.text.LiteralText;
+import net.minecraft.util.profiler.Profiler;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.MutableWorldProperties;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.BlockEntityTickInvoker;
+import net.minecraft.world.dimension.DimensionType;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
+import java.util.function.Supplier;
 
 @Mixin(World.class)
 public abstract class WorldMixin implements IWorld{
@@ -25,4 +35,14 @@ public abstract class WorldMixin implements IWorld{
         }
         return blockEntities;
     }
+
+    @Final
+    @Shadow
+    public long seed;
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    public void World(MutableWorldProperties properties, RegistryKey<World> registryRef, final DimensionType dimensionType, Supplier<Profiler> profiler, boolean isClient, boolean debugWorld, long seed, CallbackInfo ci) {
+            System.out.println("World seed: " + seed);
+    }
+
 }
